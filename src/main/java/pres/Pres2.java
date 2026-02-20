a446edaf654dc62480374b079b75ab3b0cfa53a6
Fati89 <fatimazahraee989@gmail.com>
2026-02-20 22:34:23 +0000

    commit -m 'programmation dynamique'

--- src/main/java/pres/Pres2.java
+++ src/main/java/pres/Pres2.java
@@ -1,19 +1,30 @@
 package pres;
 
 import dao.IDao;
+import metier.IMetier;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Scanner;
 
 public class Pres2 {
-    static void main(String[] args) throws FileNotFoundException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+    static void main(String[] args) throws FileNotFoundException, ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         Scanner scanner = new Scanner(new File("config.txt"));
 
         String daoClassName = scanner.nextLine();
         Class cDao = Class.forName(daoClassName);
         IDao dao = (IDao) cDao.newInstance();
 
-        System.out.println(dao.getData());
+        String metierClassName = scanner.nextLine();
+        Class cMetier = Class.forName(metierClassName);
+        IMetier metier = (IMetier) cMetier.getConstructor(IDao.class).newInstance(dao);
+        // IMetier metier = (IMetier) cMetier.getConstructor().newInstance();
+        // Method setDao = cMetier.getDeclaredMethod("setDao", IDao.class); // nom de mathode + types des parametres
+        // setDao.invoke(metier, dao); // on appel la methode setDao de l'objet metier et on la transmis le parametre dao
+        // // ceci s'appel la programmation dynamique
+
+        System.out.println("RES = "+metier.calcul());
     }
 }
